7d0cab2460cf6ef9bbb81c33e51012455fd826b9
Jennifer <chiusjennifer@gmail.com>
2024-05-22 17:40:25 +0800

    v2_邱家妤

--- app/src/main/java/tw/edu/pu/s1114859/s1114859/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1114859/s1114859/MainActivity.kt
@@ -3,13 +3,41 @@ package tw.edu.pu.s1114859.s1114859
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import tw.edu.pu.s1114859.s1114859.ui.theme.S1114859Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +50,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    Main()
                 }
             }
         }
@@ -30,17 +59,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    S1114859Theme {
-        Greeting("Android")
+fun FirstScreen(navController: NavController){
+    Column {
+        Text(text = "簡介",color= Color.Blue)
     }
 }
+
+@Composable
+fun SecondScreen(navController: NavController){
+    Column {
+        Text(text = "主要機構", color = Color.Red)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Main(){
+    val navController= rememberNavController()
+    val context= LocalContext.current
+    var showMenu by remember{ mutableStateOf(false) }
+    Column {
+        TopAppBar(title = { Image(painter = painterResource(id = R.drawable.maria), contentDescription ="logo" ) },
+            actions = {
+                IconButton(
+                    onClick = { showMenu = true }
+                ) {
+                    Icon(Icons.Default.MoreVert, contentDescription = "More")
+                }
+
+                DropdownMenu(
+                    expanded = showMenu, onDismissRequest = { showMenu = false }
+                ) {
+                    DropdownMenuItem(
+                        text = { Text("簡介") },
+                        onClick = { navController.navigate("JumpFirst")})
+
+                    DropdownMenuItem(
+                        text = { Text("主要機構") },
+                        onClick = { navController.navigate("JumpSecond")})
+                }
+
+
+            })
+        NavHost(navController = navController, startDestination = "JumpFirst")
+        {
+            composable("JumpFirst"){
+                FirstScreen(navController=navController)
+            }
+            composable("JumpSecond"){
+                SecondScreen(navController=navController)
+            }
+        }
+    }
+}
+
